35b781deefd016bc059029d25f07e9c9f95ce6b5
lavinas-science <lavinas@gmail.com>
2021-09-14 20:21:20 -0300

    cassandra 1

--- app/app.go
+++ app/app.go
@@ -1,10 +1,11 @@
 package app
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/lavinas-science/learn-oauth-api/clients/cassandra"
 	"github.com/lavinas-science/learn-oauth-api/domain/access_token"
 	"github.com/lavinas-science/learn-oauth-api/http"
 	"github.com/lavinas-science/learn-oauth-api/repository/db"
-	"github.com/gin-gonic/gin"
 )
 
 
@@ -13,9 +14,12 @@ var (
 )
 
 func StartApp() {
-	// service := access_token.NewService(db.NewRepository())
+	session, dbError := cassandra.GetSession()
+	if dbError != nil {
+		panic(dbError)
+	}
+	session.Close()
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.Run(":8080")
-
 }
--- repository/db/db_repository.go
+++ repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/lavinas-science/learn-oauth-api/clients/cassandra"
 	"github.com/lavinas-science/learn-oauth-api/domain/access_token"
 	"github.com/lavinas-science/learn-oauth-api/utils/errors"
 )
@@ -17,5 +18,10 @@ func NewRepository() DbRepository {
 }
 
 func (r *dbRepository) GetById(string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 	return nil, errors.NewInternalServerError("database not implemented")
 }
